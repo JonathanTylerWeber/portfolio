88d3f184f03f247f8c3d5e58d697be5f80df979b
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-06-20 21:29:22 +0800

    app folder names

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import Home from './pages/Home';
-import NavbarComp from './components/NavbarComp';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import NavbarComp from "./components/NavbarComp";
 
 
 function App() {
 
   return (
-    <>
-      <div className="App">
-        <BrowserRouter>
-          <NavbarComp />
-          <main>
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={<Home />}
-              />
-            </Routes>
-          </main>
-        </BrowserRouter>
-      </div>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <NavbarComp />
+        <main>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
   )
 }
 

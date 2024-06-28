a62005079847b3cfe08d79a18882c877d35ec76d
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-06-28 14:38:58 +0800

    styling

--- src/components/AboutBody.jsx
+++ src/components/AboutBody.jsx
@@ -34,7 +34,7 @@ const AboutBody = () => {
               <Col lg={6} >
                 <FadeInOnScroll threshold={.05}>
                   <p className="about-blurb">
-                    Born in Los Angeles I moved to Chengdu, China in 2018. Over the years I worked as an English Teacher and did Freelance Audio Recording and Engineering. Eventually I found my wife and when we decided to move back to the United States in 2023, I decided to start learning programming to secure a future for us. Now having completed the Springboard Software Engineering Bootcamp and currently in the long visa process for my wife, we plan on arriving in the US in early 2025. I'm looking for a remote position for now so that I can stay with the love of my life and when the time comes to finally move, I'm open to relocating for a position.
+                    Born in Los Angeles I moved to Chengdu, China in 2018. Over the years I worked as an English Teacher and did Freelance Audio Recording and Engineering. Eventually I met my wife and when we decided to move back to the United States in 2023, I decided to start learning programming to secure a future for us. Now having completed the Springboard Software Engineering Bootcamp and currently in the long visa process for my wife, we plan on arriving in the US in early 2025. I'm looking for a remote position for now so that I can stay with the love of my life and when the time comes to finally move, I'm open to relocating for a position.
                   </p>
                 </FadeInOnScroll>
               </Col>
@@ -62,7 +62,7 @@ const AboutBody = () => {
 
             <FadeInOnScroll threshold={.05}>
               <p className="about-me">
-                When I'm not programming I spend time cuddling with our 4 cats, playing DND with friends, and frequenting the nearby bouldering gym in Chengdu. Also for the past 10 years I have been making and producing my own music and videos. I've always been passionate about music whether it be playing jazz and funk or producing electronic music and hip hop. Over the past couple of years I've also learned much about video recording and editing and have been incorporating that with music. Whether it be programming or music, I'm always trying to learn new things to push myself and gain new creative tools.
+                When I'm not programming I spend time with our 4 cats, playing DND with friends, and frequenting the nearby bouldering gym in Chengdu. Also for the past 10 years I have been making and producing my own music and videos. I've always been passionate about music whether it be playing jazz and funk or producing electronic music and hip hop. Over the past couple of years I've also learned much about video recording and editing and have been incorporating that with music. Whether it be programming or music, I'm always trying to learn new things to push myself and gain new creative tools.
               </p>
             </FadeInOnScroll>
 
